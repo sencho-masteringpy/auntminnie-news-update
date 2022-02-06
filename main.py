e2b63b58a62802504bfca31d0e5620944fdde8a4
sencho <senchoparameswara@gmail.com>
2022-02-06 19:51:14 +0700

    new code

---
+++ main.py
@@ -0,0 +1,15 @@
+# retrieve the latest news from radiology website (auntminnie.com)
+def resultdata():
+    pass
+
+
+def showdata(result):
+    pass
+
+
+if __name__ == '__main__':
+    print('main apps')
+    result = resultdata()
+    showdata(result)
+
+
